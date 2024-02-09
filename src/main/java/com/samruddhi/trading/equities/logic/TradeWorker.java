f2fc91622fd308fe140f9ac93fcbe2058b283e0f
sw-pachanady <pachanady@gmail.com>
2024-02-08 21:05:47 -0800

    SA-002: Starting to add TradeApp, TradeWorker logic

--- src/main/java/com/samruddhi/trading/equities/logic/TradeWorker.java
+++ src/main/java/com/samruddhi/trading/equities/logic/TradeWorker.java
@@ -7,21 +7,49 @@ import com.samruddhi.trading.equities.domain.Bar;
 import com.samruddhi.trading.equities.domain.TradeWorkerStatus;
 import com.samruddhi.trading.equities.services.base.MarketDataService;
 import com.samruddhi.trading.equities.studies.EMACalculator;
-import core.TradingApp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.event.WindowFocusListener;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public class TradeWorker implements Callable<TradeWorkerStatus> {
 
+    private enum CurrentStatus {
+        UPTREND, DOWNTREND, CALL_HELD, PUT_HELD, NO_STATUS;
+    }
+
     private static class PreviousEmas {
         double ema5day;
         double ema13day;
         double ema50day;
+
+        public PreviousEmas(double ema5day, double ema13day, double ema50day) {
+            this.ema5day = ema5day;
+            this.ema13day = ema13day;
+            this.ema50day = ema50day;
+        }
+    }
+
+    private static class CurrentActiveTrade {
+        String ticker;
+        String optionTicker;
+        // Quantity of calls and puts bought
+        int quantity;
+        double pricePerCall;
+        double totalPrice;
+        LocalTime tradeTime;
+
+        public CurrentActiveTrade(String ticker, String optionTicker, int quantity, double pricePerCall, double totalPrice, LocalTime tradeTime) {
+            this.ticker = ticker;
+            this.optionTicker = optionTicker;
+            this.quantity = quantity;
+            this.pricePerCall = pricePerCall;
+            this.totalPrice = totalPrice;
+            this.tradeTime = tradeTime;
+        }
     }
 
     private static final Logger logger = LoggerFactory.getLogger(com.samruddhi.trading.equities.logic.TradeWorker.class);
@@ -29,11 +57,16 @@ public class TradeWorker implements Callable<TradeWorkerStatus> {
     private boolean isTerminated = false;
     private MarketDataService marketDataService;
     private String ticker;
+    /** Holds all the Bars since a buy or Call or Put is initiated until Sold */
+    private List<Bar> barsSincePurchase;
+
+    /** Holds all the Bars in the last 3 intervals (3 minutes) to see a green uptrend or a red downtrend */
+    private List<Bar> barsInTrend;
+
+    /** just the last minute's EMAs*/
     private PreviousEmas previousEmas;
 
-    private enum CurrentSTatus {
-        UPTREND, DOWNTREND, CALL_HELP, PUT_HELD, NO_STATUS;
-    }
+
 
     public TradeWorker(MarketDataService marketDataService, String ticker) {
         this.marketDataService = marketDataService;
@@ -61,8 +94,8 @@ public class TradeWorker implements Callable<TradeWorkerStatus> {
 
 
                     double ema5 = EMACalculator.calculateEMAs(dailyBars, 5);
-                    double ema13 = EMACalculator.calculateEMAs(dailyBars, 5);
-                    double ema50 = EMACalculator.calculateEMAs(dailyBars, 5);
+                    double ema13 = EMACalculator.calculateEMAs(dailyBars, 13);
+                    double ema50 = EMACalculator.calculateEMAs(dailyBars, 50);
 
                     // do the math
                     // get 50 day daily data for 50 days
